322f0e424ae5e50d47c0d767e709ad11f734ecd7
jessec01 <jegro17@gmail.com>
2025-07-17 11:48:52 -0400

    version:0.5 feat: super admin record validation

--- arbitration_detection_record/db/migrate/20250714161737_create_users.rb
+++ arbitration_detection_record/db/migrate/20250714161737_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :email, limit:200, null: false, index:true, index: {unique: true}
-      t.string :name, limit:16, null: false, index:true, index: {unique: true}
+      t.string :email, limit:200, null: false, index: {unique: true}
+      t.string :name, limit:16, null: false, index: {unique: true}
       t.timestamps
     end
   end
